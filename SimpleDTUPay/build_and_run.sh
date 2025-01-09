a6bf6504ba7630796fcbcbf3527fe0b2e9204215
Benjamin Lumbye <benjamin@lumbye.net>
2025-01-09 15:34:37 +0100

    added docker and jenkins config

---
+++ SimpleDTUPay/build_and_run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+pushd ../SimpleDTUPayServer
+mvn package
+docker compose build
+docker compose up -docker
+docker image prune -f
+popd
+
+sleep 2
+
+pushd SimpleDTUPayClient
+mvn test
+popd
